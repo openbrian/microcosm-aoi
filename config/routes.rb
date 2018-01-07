4022bd7de7a04a9ab5f17fbc3ead37e50258b124
Brian DeRocher <brian@derocher.org>
2018-01-07 01:52:46 -0500

    Home page is microcosms list page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'microcosms#index'
   get 'microcosms/:key', to: 'microcosms#show_by_key', constraints: { key: /[a-zA-Z]{1,32}/ }
   resources :microcosms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
