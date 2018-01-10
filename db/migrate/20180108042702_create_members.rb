7b3901413b2feffe0e22210641663a3a44f36420
Brian DeRocher <brian@derocher.org>
2018-01-09 22:58:19 -0500

    Add not null

--- db/migrate/20180108042702_create_members.rb
+++ db/migrate/20180108042702_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.1]
   def change
     create_table :members do |t|
-      t.integer :microcosm_id, index: true
-      t.integer :user_id, index: true
+      t.integer :microcosm_id, index: true, null: false
+      t.integer :user_id, index: true, null: false
 
       t.timestamps
     end
