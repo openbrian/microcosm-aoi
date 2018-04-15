ca61bdd2f229bd6b482406d21a2655a8b8aed585
Brian DeRocher <brian@derocher.org>
2018-04-14 21:18:52 -0400

    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 m1, = Microcosm.create!([
   {name: "MappingDC", key: "mdc2", members_num: 1, facebook: "mappingdc", twitter: "mappingdc", lat: 38.9, lon: -77.03, min_lat: 38.516 * 10**7, max_lat: 39.472 * 10**7, min_lon: -77.671 * 10**7, max_lon: -76.349 * 10**7}
 ])
-e1, Event.create!([
+e1, = Event.create!([
   {title: "Mappy Hour", moment: "2019-01-24 07:20:00", location: "Capitol City Brewing", description: "Join us for discussions of mapping.", microcosm_id: m1.id}
 ])
 u1, u2, u3 = User.create!([
