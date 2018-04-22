d7ec4f9e93ff59dd8a66824b38caac5b8297318a
Brian DeRocher <brian@derocher.org>
2018-04-21 22:42:21 -0400

    Any empty input (like facebook and twitter) should be converted to nil/null.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  before_save :normalize_blank_values
+
+  def normalize_blank_values
+    attributes.each do |column, value|
+      self[column].present? || self[column] = nil
+    end
+  end
 end
