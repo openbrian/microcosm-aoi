f0b231e3f187db32456d5650464ee37bfde13fee
Brian DeRocher <brian@derocher.org>
2018-05-01 00:28:15 -0400

    The singular form of these tables did not fail in sqlite3.

--- db/migrate/20180424231447_add_missing_references.rb
+++ db/migrate/20180424231447_add_missing_references.rb
@@ -1,11 +1,11 @@
 class AddMissingReferences < ActiveRecord::Migration[5.1]
   def change
-    add_foreign_key :events, :microcosm
-    add_foreign_key :members, :microcosm
-    add_foreign_key :members, :user
-    add_foreign_key :microcosm_changesets, :editor
-    add_foreign_key :microcosm_changesets, :microcosm
-    add_foreign_key :organizers, :microcosm
-    add_foreign_key :organizers, :user
+    add_foreign_key :events, :microcosms
+    add_foreign_key :members, :microcosms
+    add_foreign_key :members, :users
+    add_foreign_key :microcosm_changesets, :editors
+    add_foreign_key :microcosm_changesets, :microcosms
+    add_foreign_key :organizers, :microcosms
+    add_foreign_key :organizers, :users
   end
 end
