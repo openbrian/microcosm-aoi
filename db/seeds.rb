bf85a03af647a004087f6c0fbd29c5ea2795f35d
Brian DeRocher <brian@derocher.org>
2018-05-05 00:26:25 -0400

    Remove HTML from microcosm description, add provier and uid to users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 m1, = Microcosm.create!([
-  {name: "MappingDC", key: "mdc2", members_count: 0, facebook: "mappingdc", twitter: "mappingdc", lat: 38.9, lon: -77.03, min_lat: 38.516 * 10**7, max_lat: 39.472 * 10**7, min_lon: -77.671 * 10**7, max_lon: -76.349 * 10**7, description: 'MappingDC is a group organized around improving the quality of OpenStreetMap in and around Washington DC. We meet periodically for on-the-street mapping and mappy hours. See <a href="http://mappingdc.org">http://mappingdc.org</a> for more information.'}
+  {name: "MappingDC", key: "mdc2", members_count: 0, facebook: "mappingdc", twitter: "mappingdc", lat: 38.9, lon: -77.03, min_lat: 38.516 * 10**7, max_lat: 39.472 * 10**7, min_lon: -77.671 * 10**7, max_lon: -76.349 * 10**7, description: 'MappingDC is a group organized around improving the quality of OpenStreetMap in and around Washington DC. We meet periodically for on-the-street mapping and mappy hours. See http://mappingdc.org for more information.'}
 ])
 e1, = Event.create!([
   {title: "Mappy Hour", moment: "2019-01-24 07:20:00", location: "Capitol City Brewing", description: "Join us for discussions of mapping.", microcosm_id: m1.id}
 ])
 u1, u2, u3 = User.create!([
-  {name: "Brian DeRocher", provider: nil, uid: nil},
-  {name: "Steven Johnson", provider: nil, uid: nil},
-  {name: "Andrew Wiseman", provider: nil, uid: nil}
+  {name: "Brian DeRocher", provider: "osm", uid: 791438},
+  {name: "Steven Johnson", provider: "osm", uid: 25398},
+  {name: "Andrew Wiseman", provider: "osm", uid: 408282}
 ])
 Member.create!([
   {microcosm_id: m1.id, user_id: u2.id},
