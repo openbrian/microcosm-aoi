b3876f4596ae40212bcb4ddaa31c68ceb75eb072
Brian DeRocher <brian@derocher.org>
2018-06-02 21:51:10 -0400

    Make seed data match reality.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ e1, = Event.create!([
   {title: "Mappy Hour", moment: "2019-01-24 07:20:00", location: "Capitol City Brewing", description: "Join us for discussions of mapping.", microcosm_id: m1.id}
 ])
 u1, u2, u3 = User.create!([
-  {name: "OpenBrian", provider: "osm", uid: 791438},
-  {name: "seohnson", provider: "osm", uid: 25398},
-  {name: "Marion Barry", provider: "osm", uid: 408282}
+  {name: "OpenBrian", provider: "osm", uid: 791438, created_at: '2012-09-02 09:47:29'},
+  {name: "seohnson", provider: "osm", uid: 25398, created_at: '2008-02-03 21:07:53'},
+  {name: "Marion Barry", provider: "osm", uid: 408282, created_at: '2011-06-13 10:51:16'}
 ])
 Member.create!([
   {microcosm_id: m1.id, user_id: u2.id},
