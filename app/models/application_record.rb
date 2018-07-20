83eb6d6465d1efc237df8845bc9c2f98ec287339
Brian DeRocher <brian@derocher.org>
2018-07-20 10:09:32 -0400

    Describe this function better.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  before_save :normalize_blank_values
+  before_save :normalize_blank_string_values
 
-  def normalize_blank_values
+  def normalize_blank_string_values
     attributes.each do |column, value|
       self[column] = nil if self[column].kind_of? String and self[column].empty?
     end
